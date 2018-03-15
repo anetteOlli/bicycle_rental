699f8df6d878189b9c9c84c436e0e2ed31490203
Anette Olli Siiri <anettosi@stud.ntnu.no>
2018-03-15 12:24:54 +0100

    added intellij project

---
+++ TrondheimEBicycles/src/DatabaseHandler/DatabaseCleanup.java
@@ -0,0 +1,113 @@
+package DatabaseHandler;
+import java.sql.*;
+
+/**This class manages connections to MySQL.
+ *
+ * Can be replaced with try-with recources?
+ *This class is the modified version of "Opprydder.java" from "programmering
+ * i java" by Else Lervik.
+ */
+class DatabaseCleanup {
+    /**
+     *
+     * @param res
+     * @return
+     */
+    static boolean closeResult(ResultSet res) {
+        try {
+            if (res != null) {
+                res.close();
+                return true;
+            }
+        } catch (SQLException e) {
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param stm
+     */
+    static void closeSentence(Statement stm) {
+        try {
+            if (stm != null) {
+                stm.close();
+            }
+        } catch (SQLException e) {
+            skrivMelding(e, "lukkSetning()");
+        }
+    }
+
+    /**
+     *
+     * @param forbindelse
+     */
+    static void closeConnection(Connection forbindelse) {
+        try {
+            if (forbindelse != null) {
+                forbindelse.close();
+            }
+        } catch (SQLException e) {
+            skrivMelding(e, "lukkForbindelse()");
+        }
+    }
+
+    /**
+     *
+     * @param forbindelse
+     * @return
+     */
+    static boolean rollback(Connection forbindelse) {
+        try {
+            if (forbindelse != null && !forbindelse.getAutoCommit()) {
+                forbindelse.rollback();
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param con
+     * @return
+     */
+    static boolean setAutoCommit(Connection con) {
+        try {
+            if (con != null && !con.getAutoCommit()) {
+                con.setAutoCommit(true);
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param con
+     * @return
+     */
+    static boolean commit(Connection con){
+        try{
+            con.commit();
+            return true;
+        }catch (SQLException ex){
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param e
+     * @param melding
+     */
+    private static void skrivMelding(Exception e, String melding) {
+        System.err.println("*** Feil oppstått: " + melding + ". ***");
+        e.printStackTrace(System.err);
+    }
+}
---
+++ TrondheimEBicycles/src/DatabaseHandler/DatabaseConnection.java
@@ -0,0 +1,5 @@
+package DatabaseHandler;
+
+public class DatabaseConnection {
+
+}
