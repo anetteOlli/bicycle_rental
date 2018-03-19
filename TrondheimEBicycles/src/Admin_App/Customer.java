b9d486762c236ca6aa76fd0d05c68fbd435082e7
Patrick Thorkildsen <patrickt@stud.ntnu.no>
2018-03-19 19:21:13 +0100

    Changes to customer class (registerNewCustomer method)

--- TrondheimEBicycles/src/Admin_App/Customer.java
+++ TrondheimEBicycles/src/Admin_App/Customer.java
@@ -11,10 +11,11 @@ public class Customer {
     private int custPhone;
     private String custEmail;
     private String custPassword;
+    private int custCardNumber;
     private static Random random = new Random();
 
-    public Customer(int custId, String custFirstName, String custLastName, int custPhone, String custEmail, String custPassword) {
-        if(custFirstName == null || custLastName == null || custPhone == null || custEmail == null || custPassword == null){
+    public Customer(int custId, int custCardNumber, String custFirstName, String custLastName, int custPhone, String custEmail, String custPassword) {
+        if(custFirstName == null || custLastName == null || custEmail == null || custPassword == null){
             throw new IllegalArgumentException("Alle feltene må oppgis");
         }
         this.custFirstName = custFirstName.trim();
@@ -22,7 +23,8 @@ public class Customer {
         this.custPhone = custPhone;
         this.custEmail = custEmail.trim();
         this.custPassword = custPassword.trim();
-        this.custId = createNewId();
+        this.custId = custId;
+        this.custCardNumber = custCardNumber;
     }
 
     public String getName(){
@@ -37,7 +39,7 @@ public class Customer {
         return custEmail;
     }
 
-    public int createNewId(){
+   /* public int createNewId(){
         boolean numberfound = false;
         int randomnumber = 0;
         PreparedStatement selectSentence;
@@ -52,22 +54,44 @@ public class Customer {
                     numberfound = true;
                     return randomnumber;
                 }
-            }catch (SQLException e)
+            }catch (SQLException e){}
 
         }
 
-    }
+    }*/
 
-    public Customer registerNewCustomer(String firstname, String lastname, int phone, String email, String password){
-        PreparedStatement insertSentence;
+    //Customer ID skal finnes av en metode (som ikke er klar enda), tester om denne metoden funker ved å legge inn customerId manuelt
+    public Customer registerNewCustomer(int customerid, int cardNumber, String firstname, String lastname, int phone, String email, String password) {
+        PreparedStatement insertSentence = null;
         boolean ok = false;
         ResultSet res = null;
-        do{
-            try{
-
+        DatabaseCleanup databasecleanup = new DatabaseCleanup();
+        Connection connection = null;
+        do {
+            try {
+                String insertSql = "INSERT INTO Customer values(?,?,?,?,?,?,?)";
+                insertSentence = connection.prepareStatement(insertSql);
+                insertSentence.setInt(1, customerid);
+                insertSentence.setInt(2, cardNumber);
+                insertSentence.setString(3, firstname);
+                insertSentence.setString(4, lastname);
+                insertSentence.setInt(5, phone);
+                insertSentence.setString(6, email);
+                insertSentence.setString(7, password);
+                System.out.println(insertSentence);
+                insertSentence.executeUpdate();
+                ok = true;
+            } catch (SQLException e) {
+                System.out.println(e);
+            } finally {
+                databasecleanup.closeResult(res);
+                databasecleanup.closeSentence(insertSentence);
             }
-        }while(!ok);
-
-
+        } while (!ok);
+        return new Customer(customerid, cardNumber, firstname, lastname, phone, email, password);
+    }
+    public static void main(String[] args){
+        Customer customer = null;
+        customer.registerNewCustomer(1, 12, "Patrick", "Thorkildsen", 4146453, "patrick.thorkildsen@gmail.com", "passord123");
     }
 }
