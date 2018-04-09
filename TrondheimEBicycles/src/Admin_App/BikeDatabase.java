06a06f428fd4ce05ba232664adb8c095badd7731
quannt <quannt@stud.ntnu.no>
2018-04-09 14:15:52 +0200

    Removed redundant constructors

--- TrondheimEBicycles/src/Admin_App/BikeDatabase.java
+++ TrondheimEBicycles/src/Admin_App/BikeDatabase.java
@@ -10,15 +10,15 @@ public class BikeDatabase {
     Statement stm;
 
 
-    public boolean regFamily(Family newFamily) {
+    public boolean regFamily(Bicycle newBicycle) {
         try {
             cleaner.setAutoCommit(con, false);
 
             String insert2 = "INSERT INTO Bicycle(bicycle_id, powerlevel, make, model, production_date, bicycleStatus) VALUES(DEFAULT, 100, ?, 'family', ?, ?);";
             PreparedStatement RegNewFamily = connection.createPreparedStatement(con, insert2);
-            RegNewFamily.setString(1, newFamily.make);
-            RegNewFamily.setInt(2, newFamily.production_date);
-            RegNewFamily.setString(3, newFamily.bicycleStatus);
+            RegNewFamily.setString(1, newBicycle.make);
+            RegNewFamily.setInt(2, newBicycle.production_date);
+            RegNewFamily.setString(3, newBicycle.bicycleStatus);
             //RegNewBicycle.executeUpdate();
             for (int i = 0; i < 25; i++){
                 RegNewFamily.addBatch();
@@ -33,15 +33,15 @@ public class BikeDatabase {
         return false;
     }
 
-    public boolean regCargo(Cargo newCargo) {
+    public boolean regCargo(Bicycle newBicycle) {
         try {
             cleaner.setAutoCommit(con, false);
 
             String insert3 = "INSERT INTO Bicycle(bicycle_id, powerlevel, make, model, production_date, bicycleStatus) VALUES(DEFAULT, 100, ?, 'cargo', ?, ?);";
             PreparedStatement RegNewCargo = connection.createPreparedStatement(con, insert3);
-            RegNewCargo.setString(1, newCargo.make);
-            RegNewCargo.setInt(2, newCargo.production_date);
-            RegNewCargo.setString(3, newCargo.bicycleStatus);
+            RegNewCargo.setString(1, newBicycle.make);
+            RegNewCargo.setInt(2, newBicycle.production_date);
+            RegNewCargo.setString(3, newBicycle.bicycleStatus);
             //RegNewBicycle.executeUpdate();
             for (int i = 0; i < 25; i++){
                 RegNewCargo.addBatch();
@@ -56,15 +56,15 @@ public class BikeDatabase {
         return false;
     }
 
-    public boolean regRegular(Regular newRegular) {
+    public boolean regRegular(Bicycle newBicycle) {
         try {
             cleaner.setAutoCommit(con, false);
 
             String insert4 = "INSERT INTO Bicycle(bicycle_id, powerlevel, make, model, production_date, bicycleStatus) VALUES(DEFAULT, 100, ?, 'regular', ?, ?);";
             PreparedStatement RegNewRegular = connection.createPreparedStatement(con, insert4);
-            RegNewRegular.setString(1, newRegular.make);
-            RegNewRegular.setInt(2, newRegular.production_date);
-            RegNewRegular.setString(3, newRegular.bicycleStatus);
+            RegNewRegular.setString(1, newBicycle.make);
+            RegNewRegular.setInt(2, newBicycle.production_date);
+            RegNewRegular.setString(3, newBicycle.bicycleStatus);
             //RegNewBicycle.executeUpdate();
             for (int i = 0; i < 25; i++){
                 RegNewRegular.addBatch();
@@ -158,13 +158,12 @@ public class BikeDatabase {
         BikeDatabase database = new BikeDatabase();
         connection.getConnection();
 
-       //Family test1 = new Family("DBS", 19940815, "DBR");
-       //Cargo test2 = new Cargo("DBS", 19940815, "DBR");
-       //Regular test3 = new Regular("DBS", 19940815, "DBR");
+
+       //Bicycle test3 = new Bicycle("DBS", 19940815, "DBR");
        //BicycleUpdate test5 = new BicycleUpdate(8, 2, 50, "DBR", 50, 20, 5);
        //Nr_of_repairs test6 = new Nr_of_repairs(1, 20);
-       //database.regFamily(test1);
-       //database.regCargo(test2);
+       //database.regFamily(test3);
+       //database.regCargo(test3);
        //database.regRegular(test3);
        database.UpdateKM(1);
        //database.UpdateBicycle(test5);
