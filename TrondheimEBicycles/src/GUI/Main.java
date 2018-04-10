ab366ca977325104829b9536804ede3428770973
Anette Olli Siiri <anettosi@stud.ntnu.no>
2018-04-10 10:19:32 +0200

    Commit created in order to enter GUI branch.

---
+++ TrondheimEBicycles/src/GUI/Main.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    static DockFrontPage frontPage = new DockFrontPage();
+
+    public static void main(String[] args) {
+        JPanel front=  frontPage.getDockFront();
+
+
+
+
+    }
+
+
+}
