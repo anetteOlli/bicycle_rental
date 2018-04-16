b4a859666a4b3d3088c8a1f3a055cdb381f60765
Patrick Thorkildsen <patrickt@stud.ntnu.no>
2018-04-16 14:42:56 +0200

    Trying again

--- TrondheimEBicycles/src/GUI/AdministerAdmins.java
+++ TrondheimEBicycles/src/GUI/AdministerAdmins.java
@@ -37,47 +37,45 @@ public class AdministerAdmins {
 
     public AdministerAdmins() {
         list1.setModel(model);
-        try{
+        try {
             String getNames = "SELECT * FROM Employee WHERE isAdmin = 1";
             PreparedStatement names = connection.createPreparedStatement(con, getNames);
             ResultSet res = names.executeQuery();
-            while(res.next()){
+            while (res.next()) {
                 Admin admin = new Admin(res.getInt("employee_id"), res.getString("first_name"), res.getString("last_name"), res.getInt("phone"), res.getString("address"), res.getString("email"), res.getString("password"));
                 model.addElement(admin);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
 
         list1.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
-                try{
+                try {
                     Admin admin = list1.getSelectedValue();
                     boolean hired1 = true;
-                    String hiredSentence = "SELECT isHired FROM Employee WHERE employee_id = "+admin.getEmployeeId();
+                    String hiredSentence = "SELECT isHired FROM Employee WHERE employee_id = " + admin.getEmployeeId();
                     PreparedStatement hired = connection.createPreparedStatement(con, hiredSentence);
                     ResultSet res = hired.executeQuery();
-                    while(res.next()){
-                        if(res.getInt("isHired") == 1){
+                    while (res.next()) {
+                        if (res.getInt("isHired") == 1) {
                             hired1 = true;
-                        }
-                        else{
+                        } else {
                             hired1 = false;
                         }
                     }
-                    adminInfo.setText("Employee ID:"+admin.getEmployeeId());
-                    adminInfo2.setText(admin.getFirstName()+" "+admin.getLastName());
-                    adminInfo3.setText("Email: "+admin.getEmail());
-                    adminInfo4.setText("Address: "+admin.getAddress());
-                    adminInfo5.setText("Phone: "+admin.getPhone());
-                    if(hired1){
+                    adminInfo.setText("Employee ID:" + admin.getEmployeeId());
+                    adminInfo2.setText(admin.getFirstName() + " " + admin.getLastName());
+                    adminInfo3.setText("Email: " + admin.getEmail());
+                    adminInfo4.setText("Address: " + admin.getAddress());
+                    adminInfo5.setText("Phone: " + admin.getPhone());
+                    if (hired1) {
                         adminInfo6.setText("This administrator is currently employed");
-                    }
-                    else{
+                    } else {
                         adminInfo6.setText("This administrator is not currently employed");
                     }
-                }catch(Exception a){
+                } catch (Exception a) {
                     System.out.println(a.getMessage());
                 }
             }
@@ -134,30 +132,28 @@ public class AdministerAdmins {
         setAdminAsInactiveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try{
+                try {
                     Admin admin = list1.getSelectedValue();
                     String sentence;
                     boolean hired1 = true;
-                    String hiredSentence = "SELECT isHired FROM Employee WHERE employee_id = "+admin.getEmployeeId();
+                    String hiredSentence = "SELECT isHired FROM Employee WHERE employee_id = " + admin.getEmployeeId();
                     PreparedStatement hired = connection.createPreparedStatement(con, hiredSentence);
                     ResultSet res = hired.executeQuery();
-                    while(res.next()){
-                        if(res.getInt("isHired") == 1){
+                    while (res.next()) {
+                        if (res.getInt("isHired") == 1) {
                             hired1 = true;
-                        }
-                        else{
+                        } else {
                             hired1 = false;
                         }
                     }
-                    if(hired1){
-                        sentence = "UPDATE Employee SET isHired = 0 WHERE employee_id = "+admin.getEmployeeId();
-                    }
-                    else{
-                        sentence = "UPDATE Employee SET isHired = 1 WHERE employee_id = "+admin.getEmployeeId();
+                    if (hired1) {
+                        sentence = "UPDATE Employee SET isHired = 0 WHERE employee_id = " + admin.getEmployeeId();
+                    } else {
+                        sentence = "UPDATE Employee SET isHired = 1 WHERE employee_id = " + admin.getEmployeeId();
                     }
                     PreparedStatement statement = connection.createPreparedStatement(con, sentence);
                     statement.executeUpdate();
-                }catch(SQLException a){
+                } catch (SQLException a) {
                     System.out.println(a.getMessage());
                 }
             }
@@ -167,11 +163,77 @@ public class AdministerAdmins {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Administer Admins");
         frame.setContentPane(new AdministerAdmins().panel1);
-       // frame.setContentPane(new AdministerAdmins().list1);
+        // frame.setContentPane(new AdministerAdmins().list1);
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setSize(700, 500);
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        panel1 = new JPanel();
+        panel1.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(16, 3, new Insets(0, 0, 0, 0), -1, -1));
+        backButton = new JButton();
+        backButton.setText("Back");
+        panel1.add(backButton, new com.intellij.uiDesigner.core.GridConstraints(15, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_SOUTHWEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.add(panel2, new com.intellij.uiDesigner.core.GridConstraints(0, 2, 16, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        setAdminAsInactiveButton = new JButton();
+        setAdminAsInactiveButton.setText("Change activestatus of admin");
+        panel2.add(setAdminAsInactiveButton, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        addAdminButton = new JButton();
+        addAdminButton.setText("Add new admin");
+        panel2.add(addAdminButton, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        adminInfo = new JLabel();
+        adminInfo.setText("");
+        panel1.add(adminInfo, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        adminInfo2 = new JLabel();
+        adminInfo2.setText("");
+        panel1.add(adminInfo2, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        adminInfo3 = new JLabel();
+        adminInfo3.setText("");
+        panel1.add(adminInfo3, new com.intellij.uiDesigner.core.GridConstraints(2, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        adminInfo4 = new JLabel();
+        adminInfo4.setText("");
+        panel1.add(adminInfo4, new com.intellij.uiDesigner.core.GridConstraints(3, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        panel1.add(scrollPane1, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 15, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        list1 = new JList();
+        list1.putClientProperty("List.isFileList", Boolean.FALSE);
+        scrollPane1.setViewportView(list1);
+        adminInfo5 = new JLabel();
+        adminInfo5.setText("");
+        panel1.add(adminInfo5, new com.intellij.uiDesigner.core.GridConstraints(4, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.add(panel3, new com.intellij.uiDesigner.core.GridConstraints(6, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        final com.intellij.uiDesigner.core.Spacer spacer1 = new com.intellij.uiDesigner.core.Spacer();
+        panel1.add(spacer1, new com.intellij.uiDesigner.core.GridConstraints(7, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        adminInfo6 = new JLabel();
+        adminInfo6.setText("");
+        panel1.add(adminInfo6, new com.intellij.uiDesigner.core.GridConstraints(5, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return panel1;
     }
 }
--- TrondheimEBicycles/src/GUI/AdministerTechnicians.java
+++ TrondheimEBicycles/src/GUI/AdministerTechnicians.java
@@ -35,47 +35,45 @@ public class AdministerTechnicians {
 
     public AdministerTechnicians() {
         list1.setModel(model);
-        try{
+        try {
             String getNames = "SELECT * FROM Employee WHERE isAdmin = 0";
             PreparedStatement names = connection.createPreparedStatement(con, getNames);
             ResultSet res = names.executeQuery();
-            while(res.next()){
+            while (res.next()) {
                 Technician technician = new Technician(res.getInt("employee_id"), res.getString("first_name"), res.getString("last_name"), res.getInt("phone"), res.getString("address"), res.getString("email"), res.getString("password"));
                 model.addElement(technician);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
 
         list1.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
-                try{
+                try {
                     Technician technician = list1.getSelectedValue();
                     boolean hired1 = true;
-                    String hiredSentence = "SELECT isHired FROM Employee WHERE employee_id = "+technician.getEmployeeId();
+                    String hiredSentence = "SELECT isHired FROM Employee WHERE employee_id = " + technician.getEmployeeId();
                     PreparedStatement hired = connection.createPreparedStatement(con, hiredSentence);
                     ResultSet res = hired.executeQuery();
-                    while(res.next()){
-                        if(res.getInt("isHired") == 1){
+                    while (res.next()) {
+                        if (res.getInt("isHired") == 1) {
                             hired1 = true;
-                        }
-                        else{
+                        } else {
                             hired1 = false;
                         }
                     }
-                    technicianInfo.setText("Employee ID:"+technician.getEmployeeId());
-                    technicianInfo2.setText(technician.getFirstName()+" "+technician.getLastName());
-                    technicianInfo3.setText("Email: "+technician.getEmail());
-                    technicianInfo4.setText("Address: "+technician.getAddress());
-                    technicianInfo5.setText("Phone: "+technician.getPhone());
-                    if(hired1){
+                    technicianInfo.setText("Employee ID:" + technician.getEmployeeId());
+                    technicianInfo2.setText(technician.getFirstName() + " " + technician.getLastName());
+                    technicianInfo3.setText("Email: " + technician.getEmail());
+                    technicianInfo4.setText("Address: " + technician.getAddress());
+                    technicianInfo5.setText("Phone: " + technician.getPhone());
+                    if (hired1) {
                         technicianInfo6.setText("This technician is currently employed");
-                    }
-                    else{
+                    } else {
                         technicianInfo6.setText("This technician is not currently employed");
                     }
-                }catch(Exception a){
+                } catch (Exception a) {
                     System.out.println(a.getMessage());
                 }
             }
@@ -132,30 +130,28 @@ public class AdministerTechnicians {
         setTechnicianAsInactiveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try{
+                try {
                     Technician technician = list1.getSelectedValue();
                     String sentence;
                     boolean hired1 = true;
-                    String hiredSentence = "SELECT isHired FROM Employee WHERE employee_id = "+technician.getEmployeeId();
+                    String hiredSentence = "SELECT isHired FROM Employee WHERE employee_id = " + technician.getEmployeeId();
                     PreparedStatement hired = connection.createPreparedStatement(con, hiredSentence);
                     ResultSet res = hired.executeQuery();
-                    while(res.next()){
-                        if(res.getInt("isHired") == 1){
+                    while (res.next()) {
+                        if (res.getInt("isHired") == 1) {
                             hired1 = true;
-                        }
-                        else{
+                        } else {
                             hired1 = false;
                         }
                     }
-                    if(hired1){
-                        sentence = "UPDATE Employee SET isHired = 0 WHERE employee_id = "+technician.getEmployeeId();
-                    }
-                    else{
-                        sentence = "UPDATE Employee SET isHired = 1 WHERE employee_id = "+technician.getEmployeeId();
+                    if (hired1) {
+                        sentence = "UPDATE Employee SET isHired = 0 WHERE employee_id = " + technician.getEmployeeId();
+                    } else {
+                        sentence = "UPDATE Employee SET isHired = 1 WHERE employee_id = " + technician.getEmployeeId();
                     }
                     PreparedStatement statement = connection.createPreparedStatement(con, sentence);
                     statement.executeUpdate();
-                }catch(SQLException a){
+                } catch (SQLException a) {
                     System.out.println(a.getMessage());
                 }
             }
@@ -171,5 +167,67 @@ public class AdministerTechnicians {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setSize(700, 500);
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel1 = new JPanel();
+        panel1.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(16, 3, new Insets(0, 0, 0, 0), -1, -1));
+        panel2.add(panel1, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        backButton = new JButton();
+        backButton.setText("Back");
+        panel1.add(backButton, new com.intellij.uiDesigner.core.GridConstraints(15, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_SOUTHWEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.add(panel3, new com.intellij.uiDesigner.core.GridConstraints(0, 2, 16, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        setTechnicianAsInactiveButton = new JButton();
+        setTechnicianAsInactiveButton.setText("Change activestatus of technician");
+        panel3.add(setTechnicianAsInactiveButton, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        addTechnicianButton = new JButton();
+        addTechnicianButton.setText("Add new technician");
+        panel3.add(addTechnicianButton, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        technicianInfo = new JLabel();
+        technicianInfo.setText("");
+        panel1.add(technicianInfo, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        technicianInfo2 = new JLabel();
+        technicianInfo2.setText("");
+        panel1.add(technicianInfo2, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        technicianInfo3 = new JLabel();
+        technicianInfo3.setText("");
+        panel1.add(technicianInfo3, new com.intellij.uiDesigner.core.GridConstraints(2, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        technicianInfo4 = new JLabel();
+        technicianInfo4.setText("");
+        panel1.add(technicianInfo4, new com.intellij.uiDesigner.core.GridConstraints(3, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        panel1.add(scrollPane1, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 15, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        list1 = new JList();
+        list1.putClientProperty("List.isFileList", Boolean.FALSE);
+        scrollPane1.setViewportView(list1);
+        technicianInfo5 = new JLabel();
+        technicianInfo5.setText("");
+        panel1.add(technicianInfo5, new com.intellij.uiDesigner.core.GridConstraints(4, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JPanel panel4 = new JPanel();
+        panel4.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.add(panel4, new com.intellij.uiDesigner.core.GridConstraints(6, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        final com.intellij.uiDesigner.core.Spacer spacer1 = new com.intellij.uiDesigner.core.Spacer();
+        panel1.add(spacer1, new com.intellij.uiDesigner.core.GridConstraints(7, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        technicianInfo6 = new JLabel();
+        technicianInfo6.setText("");
+        panel1.add(technicianInfo6, new com.intellij.uiDesigner.core.GridConstraints(5, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     }
 }
